f381d8f6ffc9defd0b9bbd59e5395439dc5b7a03
LuisAngelSYS <70729730+LuisAngelSYS@users.noreply.github.com>
2024-05-23 00:39:34 -0500

    Orgnización Directorios por Entrega

--- script.bat
+++ script.bat
@@ -1,15 +1,8 @@
 javac DispositivoElectronico.java TV.java Computador.java
-
 javac Traccion.java TraccionMecanica.java TraccionDelantera.java TraccionTrasera.java TraccionCuatroRuedas.java Traccion4X4.java TraccionHumana.java TraccionAnimal.java TraccionVapor.java
-
 javac Transmision.java TransmisionManual.java TransmisionAutomatica.java
-
 javac MedioTransportable.java Describible.java
-
 javac Vehiculo.java VehiculoTerrestre.java VehiculoAereo.java VehiculoMaritimo.java
-
 javac Taxi.java Moto.java Bicicleta.java Barco.java Avion.java Tren.java
-
 javac Main.java
-
 java Main
